0a87b05fded7b0c7effaddfad07bf50050f7452e
kimdanny <dannykim153@gmail.com>
2022-03-15 15:36:30 +0000

    error fix + first refactoring

--- utils/configs.py
+++ utils/configs.py
@@ -1,9 +1,10 @@
 config = {
-    'base_path' : r'data',
-    'gold_path' : r'op_spam_v1.4',
-    'dec_path' : r'dec', # Both polarities have deceptive reviews from MTurk
-    'truth_path' : r'truth', # The negative polarity has authentic web reviews, positive polarity has authentic tripadvisor reviews
-    'pos_path' : r'negative_polarity',
-    'neg_path' : r'positive_polarity',
-    'amazon_path' : r'amazon_reviews'
+	'base_path': r'data',
+	'gold_path': r'op_spam_v1.4',
+	'dec_path': r'dec',  # Both polarities have deceptive reviews from MTurk
+	'truth_path': r'truth',
+	# The negative polarity has authentic web reviews, positive polarity has authentic tripadvisor reviews
+	'pos_path': r'positive_polarity',
+	'neg_path': r'negative_polarity',
+	'amazon_path': r'amazon_reviews'
 }
